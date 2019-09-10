72af33de35521e1c7190e809d5e0a7f615c1f35e
binnette <binnette@gmail.com>
2019-09-10 13:10:23 +0200

    fix image-orientation to from-image

--- script.js
+++ script.js
@@ -313,7 +313,7 @@ function publicBookCaseMap(local) {
         <div class="img-container">
         ${
           model.img
-            ? `<br /><img class="img" style="max-width:300px;max-height:300px;" src="${
+            ? `<br /><img class="img" style="max-width:300px;max-height:300px;image-orientation:from-image;" src="${
                 model.img
               }"/>`
             : ``
@@ -544,7 +544,7 @@ function publicBookCaseMap(local) {
                 contentElement.querySelector(
                   ".img-container"
                 ).innerHTML = model.img
-                  ? `<br /><img class="img" style="max-width:300px;max-height:300px;" src="${
+                  ? `<br /><img class="img" style="max-width:300px;max-height:300px;image-orientation:from-image;" src="${
                       model.img
                     }"/>`
                   : ``;
