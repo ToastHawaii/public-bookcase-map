2b089f34cf6110d69434189d1f182a61c4411a36
ToastHawaii <mar.pel@protonmail.com>
2020-02-04 23:21:05 +0100

    Add logo

--- src/filters.ts
+++ src/filters.ts
@@ -17,37 +17,24 @@ export const filters: {
     // Public bookcases
     ${nw(`["amenity"="public_bookcase"]`)}
 
-    // Reuse facility
-    ${nw(`["amenity"="reuse"][!"reuse:books"]`)}
-    ${nw(`["reuse:books"]["reuse:books"!="no"]`)}
-
-    // Library for booksharing
-    ${nw(`["library"="booksharing"]`)}
-
     // Givebox
     ${nw(`["amenity"="give_box"]`)}
-    ${nw(`["amenity"="givebox"]`)}
 
     // Library free of charge
     ${nw(`["amenity"="library"]["fee"="no"]`)}
-    
-    // Vending machine with free books
-    ${nw(`["amenity"="vending_machine"]["vending"="books"]["fee"="no"]`)}
 
     // Charity book shop
     ${nw(`["shop"="books"]["charity"="yes"]`)}
     ${nw(`["shop"="charity"]["books"]`)}
     
     // FreeStore
+    
+    ${nw(`["shop"="freestore"]`)}
     ${nw(`["shop"="charity"]["payment:none"="yes"]`)}
-    ${nw(`["shop"]["charity"="yes"]["payment:none"="yes"]`)}
     ${nw(`["shop"="second_hand"]["payment:none"="yes"]`)}
-    ${nw(`["shop"]["second_hand"="yes"]["payment:none"="yes"]`)}
 
     ${nw(`["shop"="charity"]["fee"="no"]`)}
-    ${nw(`["shop"]["charity"="yes"]["fee"="no"]`)}
-    ${nw(`["shop"="second_hand"]["fee"="no"]`)}
-    ${nw(`["shop"]["second_hand"="yes"]["fee"="no"]`)}`,
+    ${nw(`["shop"="second_hand"]["fee"="no"]`)}`,
     color: "#A0522D",
     edit: ["amenity=public_bookcase", "amenity=library", "amenity", "shop"]
   }
