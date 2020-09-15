6b855a88431a503d0b8fc92eace1132373972031
ToastHawaii <mar.pel@protonmail.com>
2020-09-15 17:05:42 +0200

    Add license header to all files

--- src/utilities/image.ts
+++ src/utilities/image.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 Markus Peloso
+// 
+// This file is part of Public bookcase map.
+// 
+// Public bookcase map is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// Public bookcase map is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with Public bookcase map.  If not, see <http://www.gnu.org/licenses/>.
+
 import md5 = require("md5");
 import { httpRegex } from "./url";
 import { startsWithIgnoreCase } from "./string";
