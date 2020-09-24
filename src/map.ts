9cdf1d9df532dedd1460673fb31857efec227203
ToastHawaii <mar.pel@protonmail.com>
2020-09-24 09:24:43 +0200

    Add random hop and move marker in front

--- src/map.ts
+++ src/map.ts
@@ -28,6 +28,8 @@ import { get, set } from "./utilities/storage";
 import { getHtmlElement, createElement } from "./utilities/html";
 import { createOverPassLayer, isIOS, shareLink } from "./createOverPassLayer";
 import { funding } from "./funding";
+import { delay, getRandomInt } from "./utilities/data";
+import { toString } from "./utilities/string";
 
 let map: L.Map;
 const layers: { [name: string]: L.Layer } = {};
@@ -346,7 +348,25 @@ function countMarkersInView(map: L.Map) {
   return count;
 }
 
-function toString(value: number, precision: number) {
-  const power = Math.pow(10, precision || 0);
-  return (Math.round(value * power) / power).toFixed(precision);
-}
+setInterval(async () => {
+  if (!document.getElementsByTagName("html")[0].classList.contains("help"))
+    return;
+
+  const markers: HTMLElement[] = [];
+  const mapBounds = map.getBounds();
+  map.eachLayer(layer => {
+    if (layer instanceof L.Marker) {
+      if (mapBounds.contains(layer.getLatLng())) {
+        markers.push((layer as L.Marker).getElement() as HTMLElement);
+      }
+    }
+  });
+
+  const marker = markers[getRandomInt(0, markers.length - 1)];
+
+  marker.style.animation = "0.4s ease-in-out 0s forwards alternate pin-top";
+
+  await delay(400);
+
+  marker.style.animation = "";
+}, 2000);
